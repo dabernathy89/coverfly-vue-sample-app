693a5c9e72616c0df2012c71706337cb500cb1ff
Daniel Abernathy <daniel@danielabernathy.com>
2022-07-02 23:51:32 -0500

    More graceful error handling

--- src/App.vue
+++ src/App.vue
@@ -6,18 +6,32 @@
 
   const input = ref(0);
   const waiting = ref(false);
+  const failed = ref(false);
 
   async function submit() {
+    failed.value = false;
     waiting.value = true;
-    await store.handleCalculation(input.value);
+    try {
+      await store.handleCalculation(input.value);
+    } catch (err) {
+      console.log({err});
+      failed.value = true;
+      input.value = 0;
+    }
     waiting.value = false;
   }
 </script>
 
 <template>
-  <label for="calculator-input">Provide your input number here:</label><br>
-  <input id="calculator-input" v-model="input" type="number" />
-  <button :disabled="waiting" @click="submit">Submit</button>
+  <form @submit.prevent="submit">
+    <label for="calculator-input">Provide your input number here:</label><br>
+    <input :disabled="waiting" id="calculator-input" v-model="input" type="number" />
+    <input type="submit" :disabled="waiting" value="Submit">
+  </form>
+
+  <p v-if="failed">
+    Please enter a number between 1 and 100.
+  </p>
 
   <hr>
 
